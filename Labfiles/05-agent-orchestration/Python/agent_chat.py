1880aff9a1313caa8a5f68edeb1de9e62c417b18
Becky Zahid <buzahid@microsoft.com>
2025-03-12 08:05:40 -0700

    Updated instructions and starter code. Added update python script

--- Labfiles/05-agent-orchestration/Python/agent_chat.py
+++ Labfiles/05-agent-orchestration/Python/agent_chat.py
@@ -1,61 +1,102 @@
 import asyncio
 import os
 
-from azure.ai.inference.aio import ChatCompletionsClient
-from azure.identity import DefaultAzureCredential
-from azure.ai.projects import AIProjectClient
-from azure.ai.projects.models import ConnectionType
-from azure.core.credentials import AzureKeyCredential
-
-from semantic_kernel import Kernel
-from semantic_kernel.agents import AgentGroupChat, ChatCompletionAgent
-from semantic_kernel.agents.strategies.selection.kernel_function_selection_strategy import KernelFunctionSelectionStrategy
-from semantic_kernel.agents.strategies.termination.kernel_function_termination_strategy import KernelFunctionTerminationStrategy
-from semantic_kernel.connectors.ai.azure_ai_inference import AzureAIInferenceChatCompletion
-from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from azure.identity.aio import DefaultAzureCredential
+from semantic_kernel.agents import AgentGroupChat
+from semantic_kernel.agents.azure_ai import AzureAIAgent, AzureAIAgentSettings
+from semantic_kernel.agents.strategies import TerminationStrategy, SequentialSelectionStrategy
 from semantic_kernel.contents.chat_message_content import ChatMessageContent
 from semantic_kernel.contents.utils.author_role import AuthorRole
-from semantic_kernel.contents.chat_history import ChatHistory
-from semantic_kernel.contents import ChatHistoryTruncationReducer
-from semantic_kernel.functions.kernel_arguments import KernelArguments
-from semantic_kernel.functions.kernel_function_from_prompt import KernelFunctionFromPrompt
 
-from devops_plugin import DevopsPlugin
 from log_file_plugin import LogFilePlugin
+from devops_plugin import DevopsPlugin
+
+INCIDENT_MANAGER = "INCIDENT_MANAGER"
+INCIDENT_MANAGER_INSTRUCTIONS = """
+Analyze the given log file or the response from the devops assistant.
+Recommend which one of the following actions should be taken:
+
+Restart service {service_name}
+Rollback transaction
+Redeploy resource {resource_name}
+Increase quota
+
+If there are no issues or if the issue has already been resolved, respond with "INCIDENT_MANAGER > No action needed."
+If none of the options resolve the issue, respond with "Escalate issue."
+
+RULES:
+- Do not perform any corrective actions yourself.
+- Read the log file on every turn.
+- Prepend your response with this text: "INCIDENT_MANAGER > {logfilepath} | "
+- Only respond with the corrective action instructions.
+"""
+
+DEVOPS_ASSISTANT = "DEVOPS_ASSISTANT"
+DEVOPS_ASSISTANT_INSTRUCTIONS = """
+Read the instructions from the INCIDENT_MANAGER and apply the appropriate resolution function. 
+Return the response as "{function_response}"
+If the instructions indicate there are no issues or actions needed, 
+take no action and respond with "No action needed."
+
+RULES:
+- Use the instructions provided.
+- Do not read any log files yourself.
+- Prepend your response with this text: "DEVOPS_ASSISTANT > "
+"""
+
+
+class ApprovalTerminationStrategy(TerminationStrategy):
+    """A strategy for determining when an agent should terminate."""
+
+    async def should_agent_terminate(self, agent, history):
+        """Check if the agent should terminate."""
+        # End the chat if the agent has indicated there is no action needed
+        return "no action needed" in history[-1].content.lower()
+
+class SelectionStrategy(SequentialSelectionStrategy):
+    """A strategy for determining which agent should take the next turn in the chat."""
+    
+    async def select_agent(self, agents, history):
+        """"Check which agent should take the next turn in the chat."""
+
+        # The Incident Manager should go after the User or the Devops Assistant
+        if (history[-1].name == DEVOPS_ASSISTANT or history[-1].role == AuthorRole.USER):
+            agent_name = INCIDENT_MANAGER
+            return next((agent for agent in agents if agent.name == agent_name), None)
+        
+        # Otherwise it is the Devops Assistant's turn
+        return next((agent for agent in agents if agent.name == DEVOPS_ASSISTANT), None)
 
 async def main():
-    # Define agent names
-    INCIDENT_MANAGER = "INCIDENT_MANAGER"
-    DEVOPS_ASSISTANT = "DEVOPS_ASSISTANT"
+    ai_agent_settings = AzureAIAgentSettings.create()
 
-    model = os.getenv("MODEL_DEPLOYMENT")
+    async with (
+        DefaultAzureCredential() as creds,
+        AzureAIAgent.create_client(credential=creds) as client,
+    ):
     
-    # Create the project client
-    project_client = AIProjectClient.from_connection_string(
-            conn_str=os.getenv("PROJECT_CONNECTION"),
-            credential=DefaultAzureCredential())
-    
-    # Get the connection properties of the Azure Open AI Service
-    connection = project_client.connections.get_default(
-        connection_type=ConnectionType.AZURE_OPEN_AI,
-        include_credentials=True
-    )
-    
-    # Use the connection information to get the endpoint for the service
-    client_endpoint = f"{str(connection.endpoint_url).strip('/')}/openai/deployments/{model}"
-    
-    # Create the chat completion service with the connection key and service endpoint
-    chat_completion_service = AzureAIInferenceChatCompletion(
-        ai_model_id=model,
-        client=ChatCompletionsClient(
-            endpoint=client_endpoint,
-            credential=AzureKeyCredential(key=connection.key),
-        ),
-        service_id="chat_completion"
-    )
+        # Create the incident manager agent on the Azure AI agent service
 
-    # Begin your code here
-    
+        # Create a Semantic Kernel agent for the Azure AI incident manager agent
+
+        # Create a chat thread to test the incident manager agent
+
+        # Create the devops agent on the Azure AI agent service
+
+        # Create a Semantic Kernel agent for the devops Azure AI agent
+
+        # Add the agents to a group chat with a custom termination and selection strategy
+        
+        for filename in os.listdir("sample_logs"):
+            logfile_msg = ChatMessageContent(role=AuthorRole.USER, content=f"USER > sample_logs/{filename}")
+            # Append the current log file to the chat
+
+            try:
+                print()
+                # Invoke a response from the agents
+                
+            except Exception as e:
+                print(f"Error during chat invocation: {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
