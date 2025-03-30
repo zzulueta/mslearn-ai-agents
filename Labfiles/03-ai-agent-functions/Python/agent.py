3674caa2494816ad76887f01cb118b9a78d08967
Graeme Malcolm <gmalc@microsoft.com>
2025-03-30 08:52:42 -0400

    Removed imports

--- Labfiles/03-ai-agent-functions/Python/agent.py
+++ Labfiles/03-ai-agent-functions/Python/agent.py
@@ -5,10 +5,6 @@ from pathlib import Path
 
 
 # Add references
-from azure.identity import DefaultAzureCredential
-from azure.ai.projects import AIProjectClient
-from azure.ai.projects.models import FunctionTool, ToolSet
-from user_functions import user_functions
 
 def main(): 
 
